2511e878c33e55d2ca897bb16ace3b47ca0733b8
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-01 18:34:28 +0000

    Update URL name for launches list endpoint to 'spacex-launches'

--- apps/launches/urls.py
+++ apps/launches/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'launches'
 
 urlpatterns = [
-    path('', views.LaunchesAPIView.as_view(), name='launches-list'),
+    path('', views.LaunchesAPIView.as_view(), name='spacex-launches'),
     path('<str:launch_id>/', views.LaunchDetailAPIView.as_view(), name='launch-detail'),
     path('health/', views.HealthCheckView.as_view(), name='health-check'),
 ]
