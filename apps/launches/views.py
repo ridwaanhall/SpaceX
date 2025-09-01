0a3a7dae6a03a20cf752aa7064da5aa9d04fd11a
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-01 18:33:04 +0000

    Remove unused import of JsonResponse in launches views

--- apps/launches/views.py
+++ apps/launches/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import JsonResponse
 from .utils import fetch_launches_data
 from .serializers import LaunchesResponseSerializer, LaunchSerializer
 import logging
