ad57a73e5617f8a3ab5ffc4d818f7c04032258b3
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-02 08:54:34 +0000

    Remove unused admin and API authentication URL patterns from urlpatterns

--- SpaceX/urls.py
+++ SpaceX/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import path, include
 from .views import APIRootView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api-auth/", include('rest_framework.urls')),
     path("", APIRootView.as_view(), name='api-root'),
     path("stats/", include('apps.stats.urls')),
     path("upcoming/", include('apps.upcoming.urls')),
