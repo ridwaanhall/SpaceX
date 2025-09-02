033f309fdd1d5ba7d1f2f6d06f9efe755528e9e0
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-02 08:41:03 +0000

    Add URL pattern for LaunchDetailAPIView to retrieve launch details by link

--- apps/launches/urls.py
+++ apps/launches/urls.py
@@ -6,4 +6,5 @@ app_name = 'launches'
 urlpatterns = [
     path('', views.LaunchesAPIView.as_view(), name='spacex-launches'),
     path('health/', views.HealthCheckView.as_view(), name='health-check'),
+    path('<str:link>/', views.LaunchDetailAPIView.as_view(), name='launch-detail'),
 ]
