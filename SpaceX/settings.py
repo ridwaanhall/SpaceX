81ee3cd974ac3355549c0e974e01e6961557cbda
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-03 04:46:34 +0000

    Update ALLOWED_HOSTS in settings to include production domains

--- SpaceX/settings.py
+++ SpaceX/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost 127.0.0.1 0.0.0.0', cast=lambda v: [s.strip() for s in v.split()])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.vercel.app .ridwaanhall.com', cast=lambda v: [s.strip() for s in v.split()])
 
 VERSION = config('API_VERSION', default='1.0.1')
 
