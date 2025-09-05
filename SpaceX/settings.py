8853858390c1ecaa950f2d5390d6f013c5e50a02
Ridwan Halim <91854659+ridwaanhall@users.noreply.github.com>
2025-09-05 10:44:22 +0000

    Add IS_AVAILABLE setting to configuration

--- SpaceX/settings.py
+++ SpaceX/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+IS_AVAILABLE = config('IS_AVAILABLE', default=True, cast=bool)
+
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.vercel.app .ridwaanhall.com', cast=lambda v: [s.strip() for s in v.split()])
 
 VERSION = config('API_VERSION', default='1.0.1')
